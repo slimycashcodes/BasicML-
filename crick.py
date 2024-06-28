1a57f8b64bbf39c75a8de198b872774ec9bd0979
Rahul Suresh <130126170+slimycashcodes@users.noreply.github.com>
2024-06-28 21:45:25 +0530

    Add files via upload

---
+++ crick.py
@@ -0,0 +1,33 @@
+#importing required modules
+import time
+import mysql.connector as ms
+
+
+#opening files and connecting to db
+fp=open('mldlff.txt','a+')
+con=ms.connect(
+    host='localhost',
+    user='root',
+    password='Welcome@1234',
+    database='cricket'
+)
+cur=con.cursor()
+
+
+#declaring variables
+finn_score=0
+finn_team=''
+sinn_score=0
+sinn_team=''
+finn_wic=0
+sinn_wic=0
+nrr=1
+dd={0:0,1:0,2:0,3:0,4:0,5:0,6:0}
+
+
+#defining function
+def tipe(s):
+     for i in s:
+         print(i,end='')
+         time.sleep(0.045)
+
